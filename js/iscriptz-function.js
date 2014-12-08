5ff1da56266ea1ef670663dc63a0530ec59d8eb3
Cebu Web Design <info@olivergonzales.com>
2014-12-08 06:46:05 -0800

    updated version
    send errors

---
+++ js/iscriptz-function.js
@@ -0,0 +1,21 @@
+function courtlink(){  window.top.location.href = localStorage['courtlink']; }
+
+
+
+function editbck() { window.history.back(); }
+
+
+
+function associates(){ window.top.location.href = "associates.html"; }
+
+
+
+function favourites(){  window.top.location.href = "load-favorites.html?url=favorites.html";}
+
+
+function requestssend() {   window.parent.location.href = "load-request.html?url=requests-send.html";  }
+function requestsrecieved() {   window.parent.location.href = "load-request.html?url=requests-recieved.html"; }
+function requestsalll() {   window.parent.location.href = "load-request.html?url=requests-all.html";  }
+
+
+function closeby() {   window.top.location.href = 'courts-list.html?'+localStorage['courtassociate'];  }
