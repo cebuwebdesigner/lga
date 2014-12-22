c36de45554d1713b14b74e1f26525a3d9314fd07
Cebu Web Design <info@olivergonzales.com>
2014-12-21 21:51:28 -0800

    update it

--- js/iscriptz-globalurl.js
+++
@@ -1,6 +0,0 @@
-//localStorage['globalurl']='http://localhost/stumblelegalapp/server/';
-
-localStorage['globalurl']='http://buildwebdesign.com/kryptonite/legalapp/';
-//var hostserver='http://buildwebdesign.com/kryptonite/legalapp/';
-
-//localStorage['globalurl']='http://appleqr.com/android/';
