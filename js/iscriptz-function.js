a0d75a71d7f702fdd1710b809ae35e54df1ba3b4
Cebu Web Design <info@olivergonzales.com>
2015-01-12 22:45:31 -0800

    tested

--- js/iscriptz-function.js
+++ js/iscriptz-function.js
@@ -17,3 +17,17 @@ function requestssend() {   window.parent.location.href = "load-request.html?url
 function requestsrecieved() {   window.parent.location.href = "load-request.html?url=requests-recieved.html"; }
 function requestsalll() {   window.parent.location.href = "load-request.html?url=requests-all.html";  }
 function closeby() {   window.top.location.href = 'courts-list.html?'+localStorage['courtassociate'];  }
+
+var hideSb = function(){
+    StatusBar.show;
+      cordova.exec(null, null, 'StatusBar', 'show', ['Ehi', 'You']);
+    };
+	
+	
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(StatusBar);
+}
+
+StatusBar.backgroundColorByName("black");
+StatusBar.backgroundColorByHexString("#000000");
