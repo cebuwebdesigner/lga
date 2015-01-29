60ad0bd6ca4c02a3978e7a2e44cce5b8843f5be4
Cebu Web Design <info@olivergonzales.com>
2015-01-28 19:46:46 -0800

    add checkgps 20sec...

---
+++ js/checkgps.js
@@ -0,0 +1,61 @@
+time=setInterval(function(){
+
+
+
+
+	
+var hostserver= localStorage['globalurl'];
+    // Wait for PhoneGap to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // PhoneGap is ready
+    //
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    // onSuccess Geolocation
+    //
+
+function onSuccess(position) {
+   	
+	
+	localStorage['mylatKey'] = position.coords.latitude;
+	localStorage['mylongKey2'] = position.coords.longitude;
+							
+	
+	//http://buildwebdesign.com/kryptonite/legalapp/gps.php?id=73&long=123&lat=10
+	
+	var thelatitude  = position.coords.latitude;
+	var thelongitude = position.coords.longitude;
+	
+	var AAA = localStorage['myID2'].split(',');
+	var theid = AAA[1];
+	
+
+	
+						 $.post(hostserver+"gps.php",
+					  {
+						id:theid,
+						long:thelongitude,
+						lat:thelatitude
+					  },
+					  function(data,status){
+						if(status=="success"){
+						//window.top.location.href = "form.html"; 
+						}else{
+							alert("Please check your connection and try again");
+							}
+					  });
+						 
+	 
+	 
+	 
+	 
+}
+
+
+
+},20000);
+
