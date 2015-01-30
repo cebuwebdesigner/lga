ab59b2c530a5cc6913255042800aee26e23ac303
Cebu Web Design <info@olivergonzales.com>
2015-01-29 16:29:59 -0800

    update for repoint host

--- js/iscriptz-globalurl.js
+++ js/iscriptz-globalurl.js
@@ -1,4 +1,4 @@
 //localStorage['globalurl']='http://appleqr.com/android/';
 //localStorage['globalurl']='http://localhost/legalapp/';
-//localStorage['globalurl']='http://buildwebdesign.com/kryptonite/legalapp/';
-localStorage['globalurl']='http://www.calloutadmin.com/sla/';
+localStorage['globalurl']='http://buildwebdesign.com/kryptonite/legalapp/';
+//localStorage['globalurl']='http://www.calloutadmin.com/sla/';
