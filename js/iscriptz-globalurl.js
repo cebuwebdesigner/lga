338224cff45a9470e2b21cd82e3ed70a754c65f3
Cebu Web Design <info@olivergonzales.com>
2015-01-29 23:10:39 -0800

    FCO updated

--- js/iscriptz-globalurl.js
+++ js/iscriptz-globalurl.js
@@ -1,4 +1,5 @@
 //localStorage['globalurl']='http://appleqr.com/android/';
 //localStorage['globalurl']='http://localhost/legalapp/';
 //localStorage['globalurl']='http://buildwebdesign.com/kryptonite/legalapp/';
+
 localStorage['globalurl']='http://www.calloutadmin.com/sla/';
